bf2ee99696a4efdde1ed2135a96b6560de9dd076
Jean THOMAS <git0@pub.jeanthomas.me>
2020-11-26 16:57:48 +0100

    Initial Commit

---
+++ mip/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  mip
+//
+//  Created by Jean THOMAS on 26/11/2020.
+//  Copyright © 2020 Jean THOMAS. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ mip/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  mip
+//
+//  Created by Jean THOMAS on 26/11/2020.
+//  Copyright © 2020 Jean THOMAS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
